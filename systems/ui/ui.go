c72157cef301ee8552e77ed19c84d885fad7e649
Rene <renenieuw@outlook.com>
2026-02-28 14:36:54 +0100

    Adding ui system.

---
+++ systems/ui/ui.go
@@ -0,0 +1,32 @@
+package uisystem
+
+import (
+	w "remapit.visualstudio.com/cubedbits/cubedbitsengine/world"
+)
+
+// UISystem sets mouse reactive components
+func UISystem(world w.World) {
+	// world.Manager.Join(world.Components.Engine.SpriteRender, world.Components.Engine.Transform, world.Components.Engine.MouseReactive).Visit(ecs.Visit(func(entity ecs.Entity) {
+	// 	sprite := world.Components.Engine.SpriteRender.Get(entity).(*c.SpriteRender)
+	// 	transform := world.Components.Engine.Transform.Get(entity).(*c.Transform)
+	// 	mouseReactive := world.Components.Engine.MouseReactive.Get(entity).(*c.MouseReactive)
+
+	// 	screenWidth := float64(world.Resources.ScreenDimensions.Width)
+	// 	screenHeight := float64(world.Resources.ScreenDimensions.Height)
+
+	// 	spriteWidth := float64(sprite.SpriteSheet.Sprites[sprite.SpriteNumber].Width)
+	// 	spriteHeight := float64(sprite.SpriteSheet.Sprites[sprite.SpriteNumber].Height)
+
+	// 	offsetX, offsetY := transform.ComputeOriginOffset(screenWidth, screenHeight)
+
+	// 	minX := (offsetX + transform.Translation.X) - spriteWidth/2
+	// 	maxX := (offsetX + transform.Translation.X) + spriteWidth/2
+	// 	minY := screenHeight - (offsetY + transform.Translation.Y) - spriteHeight/2
+	// 	maxY := screenHeight - (offsetY + transform.Translation.Y) + spriteHeight/2
+
+	// 	x, y := ebiten.CursorPosition()
+
+	// 	mouseReactive.Hovered = minX <= float64(x) && float64(x) <= maxX && minY <= float64(y) && float64(y) <= maxY
+	// 	mouseReactive.JustClicked = mouseReactive.Hovered && inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft)
+	// }))
+}
